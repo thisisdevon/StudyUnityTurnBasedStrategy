68e20c1e3f607566a6568061bfb3544249a35429
devon-sulistio <devon-sulistio@kayac.com>
2024-04-18 16:23:00 +0900

    Unit Move(+AnimationCurve)

--- Assets/Scripts/UnitScript.cs
+++ Assets/Scripts/UnitScript.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class UnitScript : MonoBehaviour
 {
+    private Vector3 moveEndPosition;
+    private Vector3 moveStartPosition;
+    private float moveTime = -1f;
+    private float elapsedTime = 0f;
+    private float moveSpeed = 4f;
+    [SerializeField] AnimationCurve moveCurve;
+
+    private void Move(Vector3 targetPosition)
+    {
+        this.moveEndPosition = targetPosition;
+        this.moveStartPosition = transform.position;
+        elapsedTime = 0f;
+
+        moveTime = Vector3.Distance(this.moveEndPosition, this.moveStartPosition) / moveSpeed;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveTime = -1f;
+        elapsedTime = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsMoving())
+        {
+            elapsedTime += Time.deltaTime;
+            float normalizedTime = elapsedTime / moveTime;
+
+            float curveEvaluate = moveCurve.Evaluate(normalizedTime);
+            Vector3 newPosition = Vector3.Lerp(this.moveStartPosition, this.moveEndPosition, curveEvaluate);
+            transform.position = newPosition;
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Move(new Vector3(4f, 0f, 4f));
+        }
+    }
+
+    bool IsMoving()
+    {
+        return elapsedTime <= moveTime;
     }
 }
