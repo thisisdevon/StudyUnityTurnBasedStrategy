593ab91b92bdf24780363d8ac382318c06b5a691
devon-sulistio <devon-sulistio@kayac.com>
2024-06-05 12:53:54 +0900

    TestDots: Jobs System

--- Assets/Scripts/TestDots/RotatingCubeSystem.cs
+++ Assets/Scripts/TestDots/RotatingCubeSystem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Transforms;
 using UnityEngine;
 
@@ -12,19 +13,36 @@ public partial struct RotatingCubeSystem : ISystem
         state.RequireForUpdate<RotateSpeed>();
     }
     
-    [BurstCompile]
     void OnUpdate(ref SystemState state)
     {
+        /*
         foreach ((RefRW<LocalTransform> localTransform, RefRO<RotateSpeed> rotateSpeed) 
             in SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotateSpeed>>())
         {
+        }
+        */
+        RotatingCubeJob rotatingCubeJob = new RotatingCubeJob
+        {
+            deltaTime = SystemAPI.Time.DeltaTime
+        };
+        rotatingCubeJob.ScheduleParallel();
+    }
+
+    [BurstCompile]
+    public partial struct RotatingCubeJob : IJobEntity
+    {
+        public float deltaTime;
+        public void Execute(ref LocalTransform localTransform, in RotateSpeed rotateSpeed)
+        {
+            
             float power = 1f;
             for (int i = 0; i < 10000; i++)
             {
                 power *= 2f;
                 power /= 2f;
             }
-            localTransform.ValueRW = localTransform.ValueRO.RotateY(rotateSpeed.ValueRO.value * SystemAPI.Time.DeltaTime * power);
+            localTransform = localTransform.RotateY(rotateSpeed.value * deltaTime * power);
+
         }
     }
 }
